fb55d1ba1b2e266da3c5acbb3ac1353d91d2bf72
namanicebed <naman.icebed@gmail.com>
2020-07-13 19:08:06 +0530

    added pinn functionality

--- src/screens/add-notes-screen/components/Header.js
+++ src/screens/add-notes-screen/components/Header.js
@@ -11,6 +11,14 @@ import moment from 'moment';
 export default Header = (props) => {
   const navigation = useNavigation();
   const [pinned, setPin] = useState(props.pinned);
+
+  const updateAsyncStorage = async () => {
+    await AsyncStorage.setItem('@user_notes', JSON.stringify(props.notes));
+    props.modalCallback(false);
+    Toast.show('Note Saved');
+    navigation.pop();
+  };
+
   const saveNotes = async () => {
     props.notes.push({
       id: props.notes.length + 1,
@@ -19,11 +27,18 @@ export default Header = (props) => {
       pinned: false,
       date: moment().format('ll'),
     });
+    updateAsyncStorage();
+  };
+
+  const saveAndPop = async () => {
+    var editedNote = {
+      ...props.notes[props.id - 1],
+      title: props.title,
+      content: props.content,
+    };
+    props.notes[props.id - 1] = editedNote;
     console.log(props.notes);
-    await AsyncStorage.setItem('@user_notes', JSON.stringify(props.notes));
-    props.modalCallback(false);
-    Toast.show('Note Saved');
-    navigation.pop();
+    updateAsyncStorage();
   };
   return (
     <View
@@ -41,7 +56,7 @@ export default Header = (props) => {
         onPress={() => {
           props.title != '' || props.content != ''
             ? props.edited
-              ? navigation.pop() //logic to save and pop
+              ? saveAndPop() //logic to save and pop
               : props.modalCallback(true)
             : navigation.pop();
         }}
@@ -50,8 +65,14 @@ export default Header = (props) => {
         <View style={{alignItems: 'center', flexDirection: 'row'}}>
           <AntDesign
             name={pinned ? 'pushpin' : 'pushpino'}
-            onPress={() => {
+            onPress={async () => {
               setPin(!pinned);
+              props.notes[props.id - 1].pinned = !pinned;
+              // console.log(!pinned);
+              await AsyncStorage.setItem(
+                '@user_notes',
+                JSON.stringify(props.notes),
+              );
               pinned ? Toast.show('Note Un Pinned') : Toast.show('Note Pinned');
             }}
             color="#1D1D1D"
